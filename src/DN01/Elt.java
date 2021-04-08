7326065c7514cbba1cf7a84ab052de0f79259f49
tGitm <timrus99@gmail.com>
2021-04-08 19:40:20 +0200

    DN01 basic class

---
+++ src/DN01/Elt.java
@@ -0,0 +1,36 @@
+package DN01;
+
+import java.util.Comparator;
+
+public class Elt implements Comparator<Elt> {
+    public int key;
+    public Object data;
+
+    //constructor
+    public Elt(int key) {
+        this.key=key;
+        data=null;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(key);
+    }
+
+    @Override
+    public int compare(Elt o1, Elt o2) {
+        return Integer.compare(o1.key, o2.key);
+    }
+}
+
+//data structure - list
+class Seznam {
+    public Seznam(Elt elt, Seznam rep) {
+
+    }
+    static Seznam insert(Seznam s, Elt e){}
+    static Elt find(Seznam s, int key){}
+    static Seznam delete(Seznam s, int key){}
+    static void printElementKeys(Seznam s){}
+    static void printElementKeyComparisons(Seznam s){}
+}
