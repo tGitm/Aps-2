71c1fd87ee2761cb1e785cf7f0d92bc7066d5b99
tGitm <timrus99@gmail.com>
2021-04-24 11:19:43 +0200

    Insert, Find, Orders

--- src/DN02/BST.java
+++ src/DN02/BST.java
@@ -14,38 +14,104 @@ class Node {
 
 public class BST {
     Node root;  //ustvarim root object
-    int occurrence = 0;
-s
+    int occurrence = 0;  //counter
+
     public BST() {
-
+        root = null;
     }
 
-    void insert(int key) {}
     boolean find(int key) {
+        /*if (root == null) return false;
+        if (root.data == key) {
+            return true;
+        }
+        if (root.data > key) {
+            return find(key);
+        }
+        return false;*/
+        while(root != null)
+        {
+            if(root.data == key) return true;
+            if(root.data > key) root = root.left;
+            else root = root.right;
+        }
 
+        return false;
     }
+
+    public void insert(int key) {
+        root = insertFunction(root, key);
+    }
+
+    Node insertFunction(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+        }
+        if (key < root.data) {
+            root.left = insertFunction(root.left, key);
+        }
+        else if (key > root.data) {
+            root.right = insertFunction(root.right, key);
+        }
+        return root;
+    }
+
+
+    //sprehodi po grafu
+    void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+            inorder(root.left);
+            System.out.println(root.data);
+            inorder(root.right);
+    }
+
+    void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data);
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    void postoder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postoder(root.left);
+        postoder(root.right);
+        System.out.println(root.data);
+    }
+
+    void printInorder() { inorder(root); }
+    void printPreorder() { preorder(root); }
+    void printPostorder() { postoder(root); }
+
 
     void delete(int key) {}
-    void printInorder() {}
-    void printPreorder() {}
-    void printPostorder() {}
     void printNodeComparisons() {}
 
 }
 
 class Run {
-    BST b = new BST();
+    public static void main(String[] args) {
+        BST b = new BST();
 
-//    b.insert(19);
-//    b.insert(11);
-//    b.insert(23);
-//    b.insert(19);
-//    b.insert(29);
-//    b.printPreorder();
+        b.insert(19);
+        b.insert(11);
+        b.insert(23);
+        b.insert(19);
+        b.insert(29);
+        System.out.println(b.find(28));
+        b.printPreorder();
 //    System.out.println("--");
 //
-//    b.printInorder();
+        b.printInorder();
 //    System.out.println("--");
 //
-//    b.printPostorder();
+        b.printPostorder();
+    }
+
 }
