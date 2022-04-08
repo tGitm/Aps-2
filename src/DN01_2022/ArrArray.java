271f9228fa8acdb721f8003e42e293468bba1413
tGitm <timrus99@gmail.com>
2022-04-08 12:49:41 +0200

    insert and find in work

--- src/DN01_2022/ArrArray.java
+++ src/DN01_2022/ArrArray.java
@@ -3,6 +3,7 @@ package DN01_2022;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@SuppressWarnings("unchecked")
 class Element {
     // existance of element -> bool
     // duplicates -> hashmap(element: numberOfDuplicates)
@@ -15,6 +16,7 @@ class Element {
     }
 }
 
+@SuppressWarnings("unchecked")
 public class ArrArray {
 
     //constructor
@@ -23,25 +25,69 @@ public class ArrArray {
     }
 
     ArrayList<ArrayList<Integer>> arrays = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> elements = new ArrayList<Integer>();
 
-    insert(int e) {
+    //TODO
+    void insert(int e) {
         ArrayList<Integer> tmp = new ArrayList<Integer>();
         tmp.add(e);
 
+        for (int i = 0; i < arrays.size(); i++) {
+            for (int j = 0; j < arrays.get(i).size() - 1; j++) {
+                if (arrays.get(i).isEmpty()){
+                    arrays.get(i).equals(tmp);
+                }
+                if (i > 0) {
+                    arrays.get(i).clear();
+                }
 
+            }
+        }
 
     }
 
-    /*
-    find(int e) {
+    //TODO
+    boolean find(int e) {
+        int min = 1000000;
+        int max = 0;
 
+        for (int i = 0; i < arrays.size(); i++) {
+            for (int j = 0; j < arrays.get(i).size() - 1; j++) {
+                if (arrays.get(i).isEmpty()) {
+                    continue;
+                }
+
+                //checking for min/max elements
+                if (j >= max) {
+                    max = j;
+                }
+                if (j <= min) {
+                    min = j;
+                }
+
+                //if e == min or max -> return true
+                if (e == min || e == max) {
+                    return true;
+                }
+                if (e < min || e > max) {
+                    continue;
+                }
+
+
+
+
+            }
+        }
+
+        return false;
     }
 
-    delete(int e) {
-
+    //TODO
+    boolean delete(int e) {
+        return false;
     }
-    */
 
+    //TODO
     void printOut() {
         if (arrays.isEmpty()) {
             System.out.println("empty");
@@ -51,9 +97,21 @@ public class ArrArray {
 
 }
 
+
 class TestClass {
     public static void main(String[] args) {
         ArrArray ar = new ArrArray();
-        ar.printOut()
-;    }
+        ar.printOut();
+
+        ar.elements.add(1);
+        ar.elements.add(11);
+        ar.elements.add(54);
+        ar.elements.add(3);
+        ar.elements.add(29);
+
+        ar.arrays.add(ar.elements);
+        System.out.println(ar.arrays);
+
+
+    }
 }
---
+++ src/DN01_2022/Item.java
@@ -0,0 +1,4 @@
+package DN01_2022;
+
+public class Item {
+}
