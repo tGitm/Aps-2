e07266ebcff0fe5181f13aa7cef0cc96b57743ff
Tim <timrus99@gmail.com>
2022-05-20 19:06:29 +0200

    resizing

--- src/DN04_2022/binheap.java
+++ src/DN04_2022/binheap.java
@@ -1,86 +1,45 @@
 package DN04_2022;
 
+
 import java.util.Arrays;
-import java.util.NoSuchElementException;
 
 public class binheap {
+    //Arrays heap;
     int[] heap;
-    int comparisons = 0;
-    int heapSize, maxSize;
+    int comparisons;
+    int index, maxSize;
 
-    private static final int FRONT = 1;
-
-    binheap(int maxSize) {
-        this.heapSize = 0;
-        this.maxSize = maxSize;
-        heap = new int[this.maxSize + 1];
-        heap[0] = Integer.MIN_VALUE;
+    binheap() {
+        this.comparisons = 0;
+        this.index = 0;
+        this.maxSize = 10;
+        heap = new int[maxSize];
     }
 
-    private int parent(int pos) {
-        return pos / 2;
+    private int stars(int pos) {
+        return (pos - 1) / 2;
     }
 
-    private int leftChild(int pos) {
-        return (2 * pos);
+    private int leviOtrok(int pos) {
+        return (pos * 2) + 1;
     }
 
-    private int rightChild(int pos) {
-        return (2 * pos) + 1;
+    private int desniOtrok(int pos) {
+        return (pos * 2) + 2;
     }
 
-    private boolean isLeaf(int pos) {
-        if (pos > (heapSize / 2) && pos <= heapSize) {
+    private boolean jeList(int pos) {
+        //if (pos <= index && pos > (index / 2)) {
+        if (desniOtrok(pos) >= maxSize || leviOtrok(pos) >= maxSize) {
+            comparisons++;
             return true;
         }
         return false;
     }
 
-    // funkcija za zamenjavo dveh node-ov v kopici
-    private void swap(int pos1, int pos2) {
-        int tmp;
-        tmp = heap[pos1];
-
-        heap[pos1] = heap[pos2];
-        heap[pos2] = tmp;
-    }
-
-    // urejanje node-a na poziciji
-    private void minHeapify(int pos) {
-        if (!isLeaf(pos)) {
-            if (heap[pos] > heap[leftChild(pos)] || heap[pos] > heap[rightChild(pos)]) {
-
-                // zamenjam z levim otrokom in uredim
-                if (heap[leftChild(pos)] < heap[rightChild(pos)]) {
-                    swap(pos, leftChild(pos));
-                    minHeapify(leftChild(pos));
-                }
-
-                // zamenjam z levim otrokom in uredim
-                else {
-                    swap(pos, rightChild(pos));
-                    minHeapify(rightChild(pos));
-                }
-            }
-        }
-    }
-
-    public void insert(int key) {
-        if (heapSize > maxSize) {
-            return;
-        }
-        heap[++heapSize] = key;
-        int current = heapSize;
-
-        while (heap[current] < heap[parent(current)]) {
-            swap(current, parent(current));
-            current = parent(current);
-        }
-    }
-
-    public Integer getMin() {
-        int minElt = 100000;
-        for (int i = 0; i < heapSize; i++) {
+    public int getMin() {
+        int minElt = Integer.MAX_VALUE;
+        for (int i = 0; i < index; i++) {
             if (heap[i] < minElt) {
                 minElt = heap[i];
             }
@@ -88,15 +47,72 @@ public class binheap {
         return minElt;
     }
 
+    // funkcija za zamenjavo dveh node-ov v kopici
+    private void swap(int pos1, int pos2) {
+        int tmpHeap;
+        tmpHeap = heap[pos1];
+
+        heap[pos1] = heap[pos2];
+        heap[pos2] = tmpHeap;
+    }
+
+    int[] resizeHeap() {
+        int[] copyHeap = Arrays.copyOf(heap, heap.length * 2);
+        heap = new int[copyHeap.length];
+        Arrays.fill(heap, 0);
+        for (int i : copyHeap) {
+            insert(i);
+        }
+        return copyHeap;
+    }
+
+    public void insert(int key) {
+        if (index >= maxSize) {
+            resizeHeap();
+        }
+        heap[index] = key;
+        int trenuten = index;
+
+        comparisons++;
+        while (heap[trenuten] < heap[stars(trenuten)]) {
+            //comparisons++;
+            swap(trenuten, stars(trenuten));
+            trenuten = stars(trenuten);
+        }
+        index++;
+    }
+
+    // urejanje node-a na poziciji
+    private void minHeapify(int pos) {
+        if (!jeList(pos)) {
+            comparisons++;
+            if (heap[pos] > heap[leviOtrok(pos)] || heap[pos] > heap[desniOtrok(pos)]) {
+                comparisons++;
+                // zamenjam z levim otrokom in uredim
+                if (heap[leviOtrok(pos)] < heap[desniOtrok(pos)]) {
+                    comparisons++;
+                    swap(pos, leviOtrok(pos));
+                    minHeapify(leviOtrok(pos));
+                }
+
+                // zamenjam z levim otrokom in uredim
+                else {
+                    swap(pos, desniOtrok(pos));
+                    minHeapify(desniOtrok(pos));
+                }
+            }
+        }
+    }
+
     // Odstrani min element iz prednostne vrste. Če je vrsta prazna, izpiše "false", sicer pa "true: <element>".
     public void deleteMin() {
-
+        int minElt = getMin();
         if (heap.length != 0) {
-            int deleted = heap[FRONT];
-            heap[FRONT] = heap[heapSize--];
-            minHeapify(FRONT);
+            int deleted = heap[0];
+            heap[0] = heap[--index];
+            minHeapify(0);
 
-            System.out.println("true: " + getMin());
+            System.out.println("true: " + minElt);
         }
         else {
             System.out.println("false");
@@ -104,13 +120,27 @@ public class binheap {
     }
 
     public void printElements() {
-        for (int i = 0; i < heapSize; i++)
-            System.out.print(heap[i] + ", ");
+        if (heap.length == 0) {
+            System.out.println("empty");
+        }
+        else {
+            for (int i = 0; i < index; i++)
+                if (i == 0) {
+                    System.out.print(heap[i]);
+                } else {
+                    System.out.print(", " + heap[i]);
+                }
+        }
         System.out.println();
     }
 
     public void printMin() {
-        System.out.println("MIN: " + getMin());
+        if (heap.length == 0) {
+            System.out.println("MIN: none");
+        }
+        else {
+            System.out.println("MIN: " + getMin());
+        }
     }
 
     public void printComparisons() {
@@ -119,13 +149,17 @@ public class binheap {
 
 }
 
-class Test {
+class BinaryHeap {
     public static void main(String[] args) {
-        binheap bh = new binheap(10);
+        binheap bh = new binheap();
         bh.insert(10); bh.insert(20); bh.insert(30);
         bh.insert(5); bh.insert(15); bh.insert(7); bh.insert(25);
-        bh.printElements(); bh.printMin(); bh.printComparisons();
+        bh.printElements();
+        bh.printMin();
+        bh.printComparisons();
         bh.deleteMin();
         bh.printElements();
+        bh.printComparisons();
+        bh.printMin();
     }
 }
