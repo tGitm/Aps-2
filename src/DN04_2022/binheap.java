e20b281f3969898610e90850f2a9335df5280226
Tim <timrus99@gmail.com>
2022-05-20 22:04:30 +0200

    4/10 points

--- src/DN04_2022/binheap.java
+++ src/DN04_2022/binheap.java
@@ -1,19 +1,21 @@
 package DN04_2022;
 
+import java.util.ArrayList;
+import java.util.Collections;
 
-import java.util.Arrays;
-
+@SuppressWarnings("unchecked")
 public class binheap {
-    //Arrays heap;
-    int[] heap;
+    //Arrays kopica;
+    //int[] kopica;
+    ArrayList<Integer> kopica = new ArrayList<>();
     int comparisons;
     int index, maxSize;
 
     binheap() {
         this.comparisons = 0;
         this.index = 0;
-        this.maxSize = 10;
-        heap = new int[maxSize];
+        //this.maxSize = 10;
+        //kopica = new int[maxSize];
     }
 
     private int stars(int pos) {
@@ -29,52 +31,42 @@ public class binheap {
     }
 
     private boolean jeList(int pos) {
-        //if (pos <= index && pos > (index / 2)) {
-        if (desniOtrok(pos) >= maxSize || leviOtrok(pos) >= maxSize) {
-            comparisons++;
+        if (desniOtrok(pos) >= kopica.size() || leviOtrok(pos) >= kopica.size()) {
             return true;
         }
         return false;
     }
 
     public int getMin() {
-        int minElt = Integer.MAX_VALUE;
+        /*int minElt = Integer.MAX_VALUE;
         for (int i = 0; i < index; i++) {
-            if (heap[i] < minElt) {
-                minElt = heap[i];
+            if (kopica[i] < minElt) {
+                minElt = kopica[i];
             }
-        }
-        return minElt;
+        }*/
+
+        return kopica.get(0);
     }
 
     // funkcija za zamenjavo dveh node-ov v kopici
     private void swap(int pos1, int pos2) {
-        int tmpHeap;
-        tmpHeap = heap[pos1];
+        //int tmpkopica;
+        //tmpkopica = kopica.get(pos1);
 
-        heap[pos1] = heap[pos2];
-        heap[pos2] = tmpHeap;
-    }
-
-    int[] resizeHeap() {
-        int[] copyHeap = Arrays.copyOf(heap, heap.length * 2);
-        heap = new int[copyHeap.length];
-        Arrays.fill(heap, 0);
-        for (int i : copyHeap) {
-            insert(i);
-        }
-        return copyHeap;
+        //kopica[pos1] = kopica[pos2];
+        //kopica.add(pos1, pos2);
+        //kopica.add(pos2, tmpkopica);
+        Collections.swap(kopica, pos1, pos2);
+        //kopica[pos2] = tmpkopica;
     }
 
     public void insert(int key) {
-        if (index >= maxSize) {
-            resizeHeap();
-        }
-        heap[index] = key;
-        int trenuten = index;
+        if (index == 0) { comparisons++; }
 
+        kopica.add(index, key);
+        int trenuten = index;
         comparisons++;
-        while (heap[trenuten] < heap[stars(trenuten)]) {
+        while (kopica.get(trenuten) < kopica.get(stars(trenuten))) {
             //comparisons++;
             swap(trenuten, stars(trenuten));
             trenuten = stars(trenuten);
@@ -83,36 +75,51 @@ public class binheap {
     }
 
     // urejanje node-a na poziciji
-    private void minHeapify(int pos) {
+    private void minkopicaify(int pos) {
         if (!jeList(pos)) {
-            comparisons++;
-            if (heap[pos] > heap[leviOtrok(pos)] || heap[pos] > heap[desniOtrok(pos)]) {
+            if (kopica.get(pos) > kopica.get(leviOtrok(pos)) || kopica.get(pos) > kopica.get(desniOtrok(pos))) {
                 comparisons++;
-                // zamenjam z levim otrokom in uredim
-                if (heap[leviOtrok(pos)] < heap[desniOtrok(pos)]) {
+                if (kopica.get(leviOtrok(pos)) <= kopica.get(desniOtrok(pos))) {
                     comparisons++;
                     swap(pos, leviOtrok(pos));
-                    minHeapify(leviOtrok(pos));
-                }
-
-                // zamenjam z levim otrokom in uredim
-                else {
+                    minkopicaify(leviOtrok(pos));
+                } else {
+                    comparisons++;
                     swap(pos, desniOtrok(pos));
-                    minHeapify(desniOtrok(pos));
+                    minkopicaify(desniOtrok(pos));
                 }
             }
         }
+
+        /*
+        if (!jeList(pos)) {
+
+            if (kopica.get(pos) > kopica.get(leviOtrok(pos)) && kopica.get(leviOtrok(pos)) < kopica.get(desniOtrok(pos))) {
+                comparisons++;
+                swap(pos, leviOtrok(pos));
+                minkopicaify(leviOtrok(pos));
+            }
+            if (kopica.get(pos) > kopica.get(desniOtrok(pos)) && kopica.get(leviOtrok(pos)) > kopica.get(desniOtrok(pos))) {
+                comparisons++;
+                swap(pos, desniOtrok(pos));
+                minkopicaify(desniOtrok(pos));
+            }
+        }
+        */
     }
 
     // Odstrani min element iz prednostne vrste. Če je vrsta prazna, izpiše "false", sicer pa "true: <element>".
     public void deleteMin() {
-        int minElt = getMin();
-        if (heap.length != 0) {
-            int deleted = heap[0];
-            heap[0] = heap[--index];
-            minHeapify(0);
+        //int minElt = getMin();
+        if (kopica.size() != 0) {
 
-            System.out.println("true: " + minElt);
+            int deleted = getMin();
+            //kopica.remove(kopica.get(0));
+            kopica.set(0, kopica.get(--index));
+            kopica.remove(index);
+            minkopicaify(0);
+
+            System.out.println("true: " + deleted);
         }
         else {
             System.out.println("false");
@@ -120,26 +127,26 @@ public class binheap {
     }
 
     public void printElements() {
-        if (heap.length == 0) {
-            System.out.println("empty");
+        if (kopica.size() != 0) {
+            for (int i = 0; i < kopica.size(); i++)
+                if (i == 0) {
+                    System.out.print(kopica.get(i));
+                } else {
+                    System.out.print(", " + kopica.get(i));
+                }
         }
         else {
-            for (int i = 0; i < index; i++)
-                if (i == 0) {
-                    System.out.print(heap[i]);
-                } else {
-                    System.out.print(", " + heap[i]);
-                }
+            System.out.print("empty");
         }
         System.out.println();
     }
 
     public void printMin() {
-        if (heap.length == 0) {
+        if (kopica.size() == 0) {
             System.out.println("MIN: none");
         }
         else {
-            System.out.println("MIN: " + getMin());
+            System.out.println("MIN: " + kopica.get(0));
         }
     }
 
@@ -149,9 +156,11 @@ public class binheap {
 
 }
 
-class BinaryHeap {
+class Binarykopica {
     public static void main(String[] args) {
         binheap bh = new binheap();
+
+        /*
         bh.insert(10); bh.insert(20); bh.insert(30);
         bh.insert(5); bh.insert(15); bh.insert(7); bh.insert(25);
         bh.printElements();
@@ -160,6 +169,27 @@ class BinaryHeap {
         bh.deleteMin();
         bh.printElements();
         bh.printComparisons();
+        */
+
+        bh.printElements();
         bh.printMin();
+        bh.printComparisons();
+        bh.insert(30); bh.insert(20); bh.insert(10);
+        bh.insert(7); bh.insert(25); bh.insert(5); bh.insert(15);
+        bh.printElements();
+        bh.printMin();
+        bh.printComparisons();
+        bh.deleteMin();
+        bh.printElements();
+        bh.printComparisons();
+        bh.deleteMin();
+        bh.printElements();
+        bh.deleteMin();
+        bh.printElements();
+        bh.deleteMin(); bh.deleteMin(); bh.deleteMin();
+        bh.printElements();
+        bh.deleteMin();
+        bh.printElements();
+        bh.printComparisons();
     }
 }
